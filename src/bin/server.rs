f5084cb47416d9e1c57bb2a5e076c4c35a981f59
sebasv <mail@sebastiaanvermeulen.nl>
2019-09-22 18:00:00 +0200

    server  app got clap

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,13 +1,37 @@
 use ::poker::run_server;
+use clap::{App, Arg};
 use std::thread;
 
-/*  TODO
-* docs
-**/
-
 fn main() -> Result<(), ()> {
+    let matches = App::new("Pokerroom server")
+        .version("0.1")
+        .author("Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>")
+        .about("A pokerroom websocket server")
+        .arg(
+            Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .value_name("URL")
+                .help("server address, defaults to 127.0.0.1")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .value_name("NUMBER")
+                .help("port to use, defaults to 2794")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let address = matches.value_of("address").unwrap_or("127.0.0.1");
+    let port = matches.value_of("port").unwrap_or("2794");
+    let loc = String::from(address) + ":" + port;
+
     let server = thread::spawn(move || {
-        run_server("127.0.0.1:2794");
+        println!("server started");
+        run_server(&loc);
     });
 
     // do not end program
