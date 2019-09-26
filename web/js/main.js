2d8a056229923e2576d4ec5580ef1605ff94684b
Nemanja Milovanovic <55963nmi@eur.nl>
2019-09-26 17:48:42 +0200

    Added toggle feature and number input for betting

---
+++ web/js/main.js
@@ -0,0 +1,11 @@
+$(".bet").click(function() {
+    $("#popup").toggle();
+});
+
+$("#popup-ok").click(function() {
+    $("#popup").toggle();
+})
+
+$("#popup-cancel").click(function() {
+    $("#popup").toggle();
+})
